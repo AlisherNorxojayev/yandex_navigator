7bfe237c6ff6ec5dccc7dd5179c01b5a7c87c99e
Alisher <norxojayevalisher3@gmail.com>
2023-01-03 14:56:16 +0500

    done

--- lib/widgets/widget.dart
+++ lib/widgets/widget.dart
@@ -10,11 +10,11 @@ class NavigatorMenu extends StatefulWidget {
 class _NavigatorMenuState extends State<NavigatorMenu> {
   List image = [
     "images/1.jpg",
-    "images/2.jpg",
-    "images/3.jpg",
-    "images/4.jpg",
-    "images/5.jpg",
-    "images/6.jpg",
+    "images/2.png",
+    "images/3.png",
+    "images/4.png",
+    "images/5.png",
+    "images/6.png",
   ];
   int i = 2;
   @override
